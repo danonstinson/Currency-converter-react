764ecb96bac47b7cb80ae71d7b8148b79b768b04
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-03-24 20:11:13 +0100

    component Result added

---
+++ src/Result/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Result = (result) => (
+    < p className="form__text">
+        Calculated value:
+        <strong>
+            {result.targetAmount.toFixed(4)}
+        </strong>
+    </p >
+);
+
+export default Result;
