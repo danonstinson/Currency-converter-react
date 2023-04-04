3bf6a3a85ecd293aa161e729b1011c43e68f5b81
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-04-04 20:00:29 +0200

    select field changed

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import "./style.css";
 import Result from "../Result";
+import currencies from "../Currencies";
 
 const Form = ({ result, calculatedResult }) => {
   const [amount, setAmount] = useState("");
-  const [rate, setRate] = useState("");
+  const [currency, setCurrency] = useState(currencies[0].prefix);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    calculatedResult(amount, rate);
+    calculatedResult(amount, currency);
 
   };
 
@@ -25,13 +26,17 @@ const Form = ({ result, calculatedResult }) => {
             <span className="form__text">Currency:
             </span>
             <select
-              className="form__field" name="currency">
-              <option value="eur-pln">EUR-PLN</option>
-              <option value="pln-eur">PLN-EUR</option>
-              <option value="usd-pln">USD-PLN</option>
-              <option value="pln-usd">PLN-USD</option>
-              <option value="eur-usd">EUR-USD</option>
-              <option value="usd-eur">USD-EUR</option>
+              className="form__field" name="currency"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option
+                  key={currency.prefix}
+                  value={currency.prefix}>
+                  {currency.prefix}
+                </option>
+              ))}
             </select>
           </label>
         </p>
@@ -44,32 +49,15 @@ const Form = ({ result, calculatedResult }) => {
               value={amount}
               className="form__field" type="number" min="0"
               required
-              placeholder="Input the amount of currency"
+              placeholder="Input the amount of currency in PLN"
               autoFocus
               onChange={({ target }) => setAmount(target.value)}
             />
           </label>
         </p>
         <p>
-          <label>
-            <span
-              className="form__text">Currency rate:
-            </span>
-            <input
-              value={rate}
-              className="form__field"
-              type="number"
-              min="0.0001"
-              step="0.0001"
-              required
-              placeholder="Input the current rate"
-              onChange={({ target }) => setRate(target.value)}
-            />
-          </label>
-        </p>
-        <p>
           <label className="form__result">
-            <Result result={result} amount={amount} rate={rate} />
+            <Result result={result} amount={amount} />
           </label>
         </p>
         <button
