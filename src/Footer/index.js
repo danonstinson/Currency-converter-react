4973b1b072aac9b7f29830d57944bf36ff1957c0
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-04-06 19:32:19 +0200

    footer updated

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./style.css";
 
 const Footer = () => (
-    <footer className="footer">
-        Copyright 2022 by Danon. All rights reserved.
-    </footer>
+  <footer className="footer">
+    Copyright 2022 by Danon. All rights reserved.
+  </footer>
 );
 
 export default Footer;
