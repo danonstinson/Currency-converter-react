8429e1f3d348763aac650406e1deafe009323a6e
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-04-17 19:40:54 +0200

    component Resu;t refactored

--- src/Container/Form/Result/index.js
+++ src/Container/Form/Result/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Result = ({ result }) => (
-    <>
-        <p className="form__text">
-            Calculated value:
-        </p >
-        <p>
-            <strong >
-                {result.result} {result.currency}
-            </strong>
-        </p>
-    </>
+  <>
+    <p className="form__text">
+      Calculated value:
+    </p>
+    <p>
+      <strong >
+        {result.result} {result.currency}
+      </strong>
+    </p>
+  </>
 );
 
 export default Result;
