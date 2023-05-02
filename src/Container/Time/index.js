af4971760a651716c9c405700756e4edad85dccf
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-05-02 19:20:23 +0200

    clock updated

--- src/Container/Time/index.js
+++ src/Container/Time/index.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 
 const Time = () => {
-  const myDate = new Date();
-  const showDate = myDate.toLocaleTimeString("pl-pl", { weekday: "long", day: "numeric", month: "long" })
+  const [myDate, setMyDate] = useState(new Date());
+
+  const showMyDate = (myDate) =>
+    myDate.toLocaleTimeString("pl-pl", { weekday: "long", day: "numeric", month: "long" })
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setMyDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [myDate]);
 
   return (
     <div
       className="timer"
     >
-      Dzisiaj jest {showDate}
+      Dzisiaj jest {showMyDate(myDate)}
     </div>
   );
 };
